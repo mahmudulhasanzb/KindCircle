cc209e2fa91833920fa23de0c2ee3a56b19435b6
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-07-12 00:58:03 +0600

    refactor: simplify HeroUI v3 CSS styling integration and remove unused provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-        {children}
-      </NextThemesProvider>
-    </HeroUIProvider>
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      {children}
+    </NextThemesProvider>
   );
 }
