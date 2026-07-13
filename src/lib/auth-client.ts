369afddfcd037b07070f81a859cb513826208859
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-07-13 22:39:42 +0600

    feat: add jwtClient plugin to client auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,9 @@
 import { createAuthClient } from 'better-auth/react';
+import { jwtClient } from 'better-auth/client/plugins';
+
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL,
-    
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  plugins: [jwtClient()],
 });
 
-export const { signIn, signUp, useSession } = createAuthClient();
+export const { signIn, signUp, useSession } = authClient;
